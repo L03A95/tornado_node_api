52dc65c74e6c5cb7cac58a70fe9f0f88b416a10e
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-03 11:18:32 -0300

    Added user validation route

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,7 +1,20 @@
 const { Router } = require('express');
 const { getAllMenus, getMenuById, postMenus, editMenus, deleteMenu } = require('../controllers/menuController.ts')
+const {APP_USER, APP_PASSWORD} = process.env
 
 const menuRouter = Router();
+
+menuRouter.post('/verify', async (req, res) => {
+    try {
+        if (req.body.username == APP_USER && req.body.password == APP_PASSWORD) {
+            res.status(200).json(true)
+        } else {
+            res.status(401).json(false)
+        }
+    } catch (error) {
+        res.status(400).json(err.message) 
+    }
+})
 
 menuRouter.get('/', async (req,res) => {
     try {
@@ -17,7 +30,7 @@ menuRouter.get('/:id', async (req, res) => {
     try{
         const result = await getMenuById(req.params.id)
         if( !result ) {
-            res.status(404).json({result: "Ese menu no existe"})
+            res.status(404).json({result: "No se ha encontrado el menu"})
         } else {
             res.status(200).json(result)
         }
@@ -38,10 +51,10 @@ menuRouter.post('/', async (req, res) => {
 menuRouter.put('/:id', async (req, res) => {
     try {
         const result = await editMenus(req.body, req.params.id)
-        if (result == [0]) {
+        if (result[0] == 0) {
             res.status(404).json({error: "No se ha encontrado el menu"})
         } else {
-            res.status(202).json(result) 
+            res.status(204).json({}) 
         }
     } catch (error) {
         res.status(400).json(error.message)
