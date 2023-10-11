a3f33bbf7ad2ddcc4ef42b99d31e8b94fc48177b
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-10 21:05:19 -0300

    Deleted all pgsql code

--- src/models/Menu.js
+++ src/models/Menu.js
@@ -12,37 +12,3 @@ const MenuSchema =  new mongoose.Schema({
 const Menu = mongoose.model('Menu', MenuSchema)
 
 module.exports = Menu
-// const { DataTypes, UUIDV4 } = require('sequelize');
-
-// module.exports = (sequelize) => {
-//     sequelize.define('menu', {
-//         id:{
-//             type: DataTypes.UUID,
-//             defaultValue: UUIDV4,
-//             primaryKey: true,
-//         },
-//         name: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//         },
-//         description: {
-//             type: DataTypes.TEXT,
-//             defaultValue: "No description..."
-//         },
-//         image: {
-//             type: DataTypes.STRING
-//         },
-//         price: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false
-//         },
-//         active: {
-//             type: DataTypes.BOOLEAN,
-//             defaultValue: true
-//         },
-//         category: {
-//             type: DataTypes.ENUM('entrada', 'parrilla', 'dulces', 'cafeteria', 'vinos'),
-//             allowNull: false
-//         }
-//     },{timestamps: false})
-// }
