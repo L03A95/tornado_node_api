bc2478bcf751e430c7f21e5552eb7da94bf0667c
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-12 04:17:02 -0300

    Changed allow origin

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const server = require('./src/app.js');
-require('./src/db.js');
 const {PORT} = process.env
 
 
--- src/db.js
+++ src/db.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
   mongoose.connect('mongodb://127.0.0.1:27017/tornadodb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }).then((db) => {console.log('Se ha conectado exitosamente a ' + db)})
+  }).then((db) => {console.log('Se ha conectado exitosamente a MongoDB')})
   .catch((err) => console.log(err))
