9600bc405cf2b488826ddc2c1ada69d6b253d889
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-17 12:06:48 -0300

    Deleted clodinary

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,47 +1,9 @@
 const { Router } = require('express');
 const { getAllMenus, getMenuById, postMenus, editMenus, deleteMenu } = require('../controllers/menuController.js')
 const {APP_USER, APP_PASSWORD} = process.env
-const multer = require('multer');
-const {v2} = require('cloudinary')
-
-const cloudinary = v2
-          
-cloudinary.config({ 
-  cloud_name: 'dlaqpndlk', 
-  api_key: '945541274585366', 
-  api_secret: 'ffMXS9C9-76dCwmWnEFQnc-aMZQ' 
-});
 
 const menuRouter = Router();
 
-// const upload = multer({ dest: 'uploads/' });
-
-menuRouter.post('/upload', async (req, res) => {
-    try {
-      // Accede al archivo de la imagen directamente desde req.files
-      console.log(req.body)
-      if (!req.files || !req.files.image) {
-        console.log(req.files)
-        return res.status(404).json({ error: 'No se recibió ningún archivo' });
-      }
-  
-      const image = req.files.image;
-  
-      // Cargar el archivo en Cloudinary
-      cloudinary.v2.uploader.upload(image.tempFilePath, { public_id: "menu" }, function (error, result) {
-        if (error) {
-          return res.status(400).json({ error: 'Error al cargar la imagen en Cloudinary' });
-        }
-  
-        const imageUrl = result.secure_url;
-  
-        // Responder con la URL de la imagen en Cloudinary
-        res.status(200).json(imageUrl);
-      });
-    } catch (error) {
-      res.status(400).json(error.message);
-    }
-  });
  
 menuRouter.post('/verify', async (req, res) => {
     try {
